b5169d70845f0fc47a3735e29f663dc1237b7052
Ksavir <kevinsavir20@gmail.com>
2025-07-12 16:05:43 +0200

    listado de pokemon, con animaciones de entrada, pagina prev and next

---
+++ src/consts/site-info.ts
@@ -0,0 +1,6 @@
+export const siteInfo = {
+    title: "Pokemon Static",
+    description: "A static site for Pokemon sprites and data",
+    url: "https://pokemon-static.vercel.app/",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
+};
